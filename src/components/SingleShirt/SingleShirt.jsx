6b9937529614db9a307ac877c2805c4e0dc3feaa
Mahadi90 <mahadimhs787@gmail.com>
2023-04-07 19:47:20 +0600

    shirt-card style added

--- src/components/SingleShirt/SingleShirt.jsx
+++ src/components/SingleShirt/SingleShirt.jsx
@@ -7,6 +7,10 @@ const SingleShirt = ({singleShirt}) => {
     return (
         <div className='shirt-card'>
             <img src={picture} alt="" />
+            <h3>{name}</h3>
+            <p>Price: ${price}</p>
+            <p>Gender: {gender}</p>
+            <button className='shirt-btn'>Buy Now</button>
         </div>
     );
 };
